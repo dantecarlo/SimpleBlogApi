415f70a1035e5a5a02c6a6b214a4ef7ec380255c
dantecarlo <carlo_cnp@hotmail.com>
2019-05-05 22:33:24 -0500

    ADD custom fields in querys

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Types::PostType < Types::BaseObject
-    description 'A post of an user'
+  description 'A post of an user'
+
+  field :id, ID, null: false
+  field :user_id, Int, null: true, camelize: false
+  field :body, String, null: true
   
-    field :id, ID, null: false
-    field :user_id, Int, null: true, camelize: false
-    field :body, String, null: true
-  end
-  
+  field :comments, [Types::CommentType], null: true
+end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -11,4 +11,22 @@ class Types::UserType < Types::BaseObject
   field :city, String, null: true
   field :postcode, Int, null: true
   field :country, String, null: true
+
+  field :posts, [Types::PostType], null: true
+
+  field :full_name, String, null: true, camelize: false
+
+  def full_name
+    [object.first_name, object.last_name].compact.join ' '
+  end
+
+  field :adress, String, null: true
+
+  def address
+    ([:street, :number, :postcode, :country].map do |a|
+      object.send(a)&.strip
+    end.compact - ['']).join(', ')
+  end
+
+
 end
